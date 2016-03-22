788f6dfbb37383ed12708f310a0b22905af5acf3
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-03-22 09:09:57 +0900

    Support: `make package`

--- make.cmd
+++ make.cmd
@@ -1,9 +1,21 @@
 setlocal
+set "ARCHS=amd64 386"
 
-for %%I in (amd64 386) do (
-    set "GOARCH=%%I"
-    mkdir "%%I" 2>nul
-    go build -o "%%I/sponge.exe"
-)
+if not "%1" == "" goto %1
+
+    for %%I in (%ARCHS%) do (
+        set "GOARCH=%%I"
+        mkdir "%%I" 2>nul
+        go build -o "%%I/sponge.exe"
+    )
+    goto end
+
+:package
+    for %%I in (%ARCHS%) do (
+        zip -j sponge-%DATE:/=%-%%I.zip readme.md %%I\sponge.exe 
+    )
+    goto end
+
+:end
 
 endlocal
