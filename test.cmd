88a21229f9ebe4f010e81fa73b6b7f1de72eef82
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-01-08 22:33:51 +0900

    Improve test.cmd

--- test.cmd
+++ test.cmd
@@ -1,6 +1,19 @@
-echo a>foo.txt
-echo b>>foo.txt
-echo c>>foo.txt
-echo d>>foo.txt
-cat -n foo.txt | sponge foo.txt
-type foo.txt
+@echo off
+setlocal
+gawk "BEGIN{ for ( i=0 ; i<50000 ; i++ ){ printf \"%%d \",i } }" > original.txt
+REM pause
+call :testone cat -n || exit /b 1
+call :testone gawk "{ print NR,$0 }" || exit /b 1
+del original.txt
+endlocal
+exit /b 0
+
+:testone
+    echo TEST CASE: %*
+    %* original.txt > expect.txt
+    %* original.txt | .\sponge original.txt result.txt
+    comp expect.txt original.txt /M || exit /b 1
+    comp result.txt original.txt /M || exit /b 2
+    del expect.txt result.txt
+    rem type original.txt
+    exit /b 0
